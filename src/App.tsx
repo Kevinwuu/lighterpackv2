fb62046d7c01d700c7f5e05372dd2c36f5227828
Ethan Wu <405417048@gms.tku.edu.tw>
2025-02-03 23:33:04 +0800

    chore: remove some default code from CRA

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import {
   SelectValueText,
   createListCollection,
 } from "@chakra-ui/react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
 
 const frameworks = createListCollection({
@@ -23,64 +21,19 @@ const frameworks = createListCollection({
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <SelectRoot collection={frameworks} size="sm" width="320px">
-          <SelectLabel>Select framework</SelectLabel>
-          <SelectTrigger>
-            <SelectValueText placeholder="Select movie" />
-          </SelectTrigger>
-          <SelectContent>
-            {frameworks.items.map((movie) => (
-              <SelectItem item={movie} key={movie.value}>
-                {movie.label}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </SelectRoot>
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <SelectRoot collection={frameworks} size="sm" width="320px">
+        <SelectLabel>Select framework</SelectLabel>
+        <SelectTrigger>
+          <SelectValueText placeholder="Select movie" />
+        </SelectTrigger>
+        <SelectContent>
+          {frameworks.items.map((movie) => (
+            <SelectItem item={movie} key={movie.value}>
+              {movie.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </SelectRoot>
     </div>
   );
 }
